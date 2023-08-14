49334295c48f8f5cf8d2504b2dd4f3763cac1094
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-08-14 15:49:15 -0500

    add logo in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,20 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
 interface HeaderProps {}
 
 export default function Header({}: HeaderProps) {
-  return <div>Header</div>;
+  return (
+    <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-slate-200 px-6 py-3 ">
+      <Link href="/">
+        <Image
+          src="/assets/sunflower.png"
+          width={100}
+          height={100}
+          alt="Logo"
+        />
+      </Link>
+    </nav>
+  );
 }
