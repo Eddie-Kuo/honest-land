0eccd1a3f2f7c841ace3f6f6cbcacc6f480e38bd
Eddie-Kuo <Eddiekuo97@gmail.com>
2023-08-14 15:58:13 -0500

    add name into header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,14 +6,18 @@ interface HeaderProps {}
 
 export default function Header({}: HeaderProps) {
   return (
-    <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-slate-200 px-6 py-3 ">
-      <Link href="/">
+    <nav className="fixed top-0 z-30 flex w-full items-center justify-between bg-yellow-200/30 px-6 py-3 ">
+      <Link href="/" className="flex items-center">
         <Image
           src="/assets/sunflower.png"
-          width={100}
+          width={150}
           height={100}
           alt="Logo"
         />
+        <p className="text-yl text-2xl font-light text-yellow-500 max-sm:hidden">
+          HONEST <br />
+          LAND
+        </p>
       </Link>
     </nav>
   );
